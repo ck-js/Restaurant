31a941132718808cd058c2e5d615aae27dd1897e
ck-js <ckentertainmentinfo@gmail.com>
2024-01-13 22:41:22 +0700

    Refactor our function constructor to an object constructor that has a function to create an object instance and adds it to the menu items array so we can easily call the function and pass the food item title, description and price and it creates a object instance and adds to a array. We then create a function to loop through the array and it creates html elements to store the objects values which is then appended to the parent container which is created in the functions local scope.

--- src/breakfast.js
+++ src/breakfast.js
@@ -1,8 +1,3 @@
-// create object constructor 
-function MenuItem() {
-
-}
-
 
 function breakfastHeading() {
     const h1Container = document.createElement('div');
@@ -13,7 +8,78 @@ h1Container.append(h1)
 return h1Container;
 
 }
+
+
+// create object constructor 
+function MenuItem(title,description,price,vegan) {
+this.title = title,
+this.description = description;
+this.price = price;
+this.vegan = false;
+}
+// create empty array to store menu item object instances
+let menuItemsArray = [];
+
+// function to create new menu item and add to array
+function createMenuItemInstance(title, description, price,vegan) {
+const item = new MenuItem(title,description,price,vegan);
+menuItemsArray.push(item)
+}
+createMenuItemInstance('Chicken Tikka Pasta Salad', 'Screeamy', 200)
+createMenuItemInstance('Greek Salad Salad', 'Crunchy', 200)
+
+
+// create function to loop through menu items array and append to html elements
+function loopMenuItems() {
+    const menuItemContainer = document.createElement('div')   
+    menuItemContainer.classList.add('menu-item')
+for (let i = 0; i < menuItemsArray.length; i++) {
+    const element = menuItemsArray[i];
+
+    const menuItemTitle = document.createElement('h4');
+    const menuItemDescription = document.createElement('p');
+    const menuItemPrice = document.createElement('p');
+
+    menuItemTitle.innerHTML = element.title;
+    menuItemDescription.innerHTML =element.description;
+    menuItemPrice.innerHTML = element.price;
+    
+    menuItemContainer.appendChild(menuItemTitle)
+    menuItemContainer.appendChild(menuItemDescription)
+    menuItemContainer.appendChild(menuItemPrice)
+    
+}
+return menuItemContainer;
+
+
+}
+
+
+// function addMenuItem(title, description, price) {
+
+
+//     menuItemContainer.classList.add('menu-item')
+//     const menuItemTitle = document.createElement('h4');
+//     const menuItemDescription = document.createElement('p');
+//     const menuItemPrice = document.createElement('p');
+//     menuItemTitle.innerHTML = burger.title;
+//     menuItemDescription.innerHTML =burger.description;
+//     menuItemPrice.innerHTML = price
+    
+//     menuItemContainer.appendChild(menuItemTitle)
+//     menuItemContainer.appendChild(menuItemDescription)
+//     menuItemContainer.appendChild(menuItemPrice)
+
+
+    
+//     return menuItemContainer;
+// }
+
+
+
 function bakedSelection() {
+    console.log(menuItemsArray[0]);    
+    console.log(menuItemsArray[1]);
     const parentContainer = document.createElement('div');
     parentContainer.id = 'baked-selection-container';
 const servedDaily = document.createElement('h2');
@@ -21,54 +87,43 @@ servedDaily.innerHTML = 'Breakfast Served Daily'
 const bakedSelectionTitle = document.createElement('h1');
 bakedSelectionTitle.innerHTML = 'Baked Selection ( 7:00 - 16:00 )';
 
-const item1 = addMenuItem('CLASSIC SCONES (BAKED DAILY)',
-'two warmed scones layered with fresh cream & homemade berry compote',
-150);
-const item2 = addMenuItem('CROISSANT (PLAIN)',
-'add butter & homemade berry compote +40',
-70 );
-const item3 = addMenuItem(
-'Dark Chocolate Croissant',
-' Cocoa 60% dark chocolate ganache, hazelnut crumble',
-120
-);
-const item4 = addMenuItem(
-    'Almond Croissant',
-    '// homemade almond créme, toasted flaked almonds',
-    120
-)
 
 
 
 
 
+// const item2 = addMenuItem('CROISSANT (PLAIN)',
+// 'add butter & homemade berry compote +40',
+// 70 );
+// const item3 = addMenuItem(
+// 'Dark Chocolate Croissant',
+// ' Cocoa 60% dark chocolate ganache, hazelnut crumble',
+// 120
+// );
+// const item4 = addMenuItem(
+//     'Almond Croissant',
+//     '// homemade almond créme, toasted flaked almonds',
+//     120
+// )
+
+
+// append baked selection menu items to parent container
 parentContainer.appendChild(servedDaily);
 parentContainer.appendChild(bakedSelectionTitle);
-parentContainer.appendChild(item1)
-parentContainer.appendChild(item2)
-parentContainer.appendChild(item3)
-parentContainer.appendChild(item4)
+parentContainer.appendChild(loopMenuItems())
+// parentContainer.appendChild(item1)
+// parentContainer.appendChild(item2)
+// parentContainer.appendChild(item3)
+// parentContainer.appendChild(item4)
 
 return parentContainer;
 }
 
-function addMenuItem(title, description, price) {
 
-    const menuItemContainer = document.createElement('div');
-    menuItemContainer.classList.add('menu-item')
-    const menuItemTitle = document.createElement('h4');
-    const menuItemDescription = document.createElement('p');
-    const menuItemPrice = document.createElement('p');
-    menuItemTitle.innerHTML = title;
-    menuItemDescription.innerHTML = description;
-    menuItemPrice.innerHTML = price
-    
-    menuItemContainer.appendChild(menuItemTitle)
-    menuItemContainer.appendChild(menuItemDescription)
-    menuItemContainer.appendChild(menuItemPrice)
 
-    return menuItemContainer;
-}
+
+
+
 
 
 
